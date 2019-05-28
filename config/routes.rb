a9e9276b29906a18180d1acdb910fee7da15bc2a
sochisanchan <sochi19990403@gmail.com>
2019-05-28 16:57:29 +0900

    revision_front_page_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+resources :messages,only: [:index]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
