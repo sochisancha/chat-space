f0171c31c33305121831f4721b767f3fd0641ec4
sochisanchan <sochi19990403@gmail.com>
2019-06-01 14:47:26 +0900

    revision_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+    devise_for :users
+    root 'messages#index'
+    resources :users, only: [:index, :edit, :update]
+    resources :groups, only: [:new, :create, :edit, :update] do
+      resources :messages, only: [:index, :create]
+    end
+  end
   
-  resources :messages,only: [:index]
-end
+
