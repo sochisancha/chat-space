9974074d493bfc99e6ec8b35d9e7c378eefb4673
sochisanchan <sochi19990403@gmail.com>
2019-06-03 20:53:05 +0900

    all

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,42 @@
 class GroupsController < ApplicationController
+  
+  before_action :set_group, only: [:edit, :update]
+  
+  def index
+  end
   def new
-    @users = User.all
     @group = Group.new
-  end
-
-  def edit
-  end
-  def update
+    @group.users << current_user
+    # binding.pryß
   end
   def create
+    @group = Group.new(create_params)
+    if @group.save
+    redirect_to root_path, notice: "グループを作成しました。"
+    else
+      render :new 
+    end
   end
+  def edit
+   
+  end
+  def update
+    if @group.update(create_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+  
 
   private
+  
+  def create_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+  def set_group
+    @group = Group.find(params[:id])
+  end
 
 
 
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,15 @@
 class Group < ApplicationRecord
   has_many :users, through: :members
-  has_many :messages
   has_many :members
+  has_many :messages
+  
+
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.text? ? last_message.text : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
