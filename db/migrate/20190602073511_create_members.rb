b20228b9352e937366308bd496597a400345befa
sochisanchan <sochi19990403@gmail.com>
2019-06-14 20:29:14 +0900

    s3

--- db/migrate/20190602073511_create_members.rb
+++ db/migrate/20190602073511_create_members.rb
@@ -1,7 +1,7 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-      t.references :user,  foreign_key: true
+      t.references :user, foreign_key: true
       t.references :group, foreign_key: true
       t.timestamps
     end
