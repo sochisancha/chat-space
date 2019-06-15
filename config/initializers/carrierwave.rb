9317bd54dc36366373f03f94a288702a94cba847
sochisanchan <sochi19990403@gmail.com>
2019-06-15 14:24:29 +0900

    ｓ３修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'sandasoichiro-chat-space'
-  config.asset_host = 'https://sandasoichiro-chat-space.s3-ap-northeast-1.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sandasoichiro-chat-space'
 end
